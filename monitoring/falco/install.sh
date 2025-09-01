38faee397fec4c65904cae85fc15b0c37afbfc17
tom1299 <tom1299@jeeatwork.com>
2025-09-01 12:49:17 +0200

    Enhance falco helm deployment add kubesec sast command

--- monitoring/falco/install.sh
+++ monitoring/falco/install.sh
@@ -1,9 +1,12 @@
 helm repo add falcosecurity https://falcosecurity.github.io/charts
 helm repo update
 
+helm uninstall falco -n falco --ignore-not-found
+
 helm install falco falcosecurity/falco \
     --create-namespace \
     --namespace falco \
-    --set driver.kind=ebpf
+    --set driver.kind=ebpf \
+    --values ./falco-values.yaml
 
 kubectl wait --for=condition=Ready pods -l app.kubernetes.io/name=falco -n falco --timeout=300s
---
+++ supply-chain/images/README.md
@@ -0,0 +1,4 @@
+# Using kubesec-sast
+```bash
+docker run -i kubesec/kubesec:latest scan /dev/stdin < ./kindnet-pod.yaml
+```
