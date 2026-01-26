bc47f124b3c1acd9a3f94cd3c35fa56be6b05e40
tom1299 <tom1299@jeeatwork.com>
2026-01-26 08:21:18 +0100

    Add very basic pod agent to test new return types of pod functions

---
+++ kubernetes-tools/src/kubernetes_agents/pod_agent.py
@@ -0,0 +1,38 @@
+from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import InMemorySaver
+
+from kubernetes_tools.pods import (
+    get_pods_by_labels_as_dict
+)
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SYSTEM_PROMPT = """
+You are a Kubernetes Pod Agent. Your role is to retrieve Pods information
+based on label selector and namespace. You have access to the following tools:
+
+* Retrieve pod information by labels and namespace
+"""
+
+model = init_chat_model(
+    "claude-sonnet-4-5-20250929",
+    temperature=0,
+    timeout=60,
+    max_tokens=4000
+)
+
+checkpointer = InMemorySaver()
+
+tools = [
+    get_pods_by_labels_as_dict
+]
+
+agent = create_agent(
+    model=model,
+    system_prompt=SYSTEM_PROMPT,
+    tools=tools,
+    checkpointer=checkpointer
+)
---
+++ kubernetes-tools/tests/unit/kubernetes_agents/test_pod_agent.py
@@ -0,0 +1,33 @@
+import uuid
+
+from langchain_core.messages import AIMessage
+from kubernetes_agents.pod_agent import agent
+
+class TestPodAgent:
+
+    def test_get_pod_information_for_existing_pd(self):
+        config = {"configurable": {"thread_id": f"test-{uuid.uuid4()}"}}
+
+        response = agent.invoke(
+            {"messages": [{"role": "user", "content": "Give me all image names of containers in the pod "
+                                                      "labeled 'app: backend' in namespace test-app."
+                                                      "Do not include ephemeral containers."}]},
+            config=config
+        )
+
+        get_pods_tool_invoked = False
+        for message in response["messages"]:
+            if isinstance(message, AIMessage) and not get_pods_tool_invoked:
+                ai_message: AIMessage = message
+                assert ai_message.tool_calls is not None
+                assert len(ai_message.tool_calls) ==1
+                tool_call = ai_message.tool_calls[0]
+                assert tool_call["name"] == "get_pods_by_labels_as_dict"
+                tool_call_args = tool_call["args"]
+                assert tool_call_args["labels"] == {"app": "backend"}
+                assert tool_call_args["namespace"] == "test-app"
+                get_pods_tool_invoked = True
+            elif isinstance(message, AIMessage) and message.content is not None:
+                assert "python:3.9-slim" in message.content
+
+        assert get_pods_tool_invoked, "Expected get_pods_by_labels_as_dict tool to be invoked"
